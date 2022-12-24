de5c1d8ead371e1e49f9823dca9043058312e5ba
drili <DriliB@hotmail.com>
2022-12-25 00:43:25 +0100

    :hammer: Authentication and auth + asset files

--- server/index.js
+++ server/index.js
@@ -11,6 +11,12 @@ import morgan from "morgan"
 import path from "path"
 import { fileURLToPath } from "url"
 
+// * ROUTES
+import authRoutes from "./routes/auth.js"
+
+// * CONTROLLERS
+import { register } from "./controllers/auth.js"
+
 // * CONFIGURATIONS
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -36,6 +42,12 @@ const storage = multer.diskStorage({
 })
 
 const upload = multer({ storage })
+
+// * ROUTES WITH FILES
+app.post("/auth/register", upload.single("picture"), register)
+
+// * ROUTES
+app.use("/auth", authRoutes)
 
 // * MONGOOSE SETUP
 const PORT = process.env.PORT || 6001
