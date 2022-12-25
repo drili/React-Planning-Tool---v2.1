bff48160e50f77b02547397448b6f0d81ac9bcd8
drili <DriliB@hotmail.com>
2022-12-25 13:58:43 +0100

    :hammer: React redux and redux toolkit setup

--- client/src/App.js
+++ client/src/App.js
@@ -1,9 +1,18 @@
-import './App.css';
+import { BrowserRouter, Navigate, Routes, Route } from "react-router-dom";
+import HomePage from "scenes/homePage";
+import LoginPage from "scenes/loginPage";
+import ProfilePage from "scenes/profilePage";
 
 function App() {
     return (
         <div className="app">
-
+            <BrowserRouter>
+                <Routes>
+                    <Route path="/" element={<LoginPage></LoginPage>}></Route>
+                    <Route path="/home" element={<HomePage></HomePage>}></Route>
+                    <Route path="/profile/:userId" element={<ProfilePage></ProfilePage>}></Route>
+                </Routes>
+            </BrowserRouter>
         </div>
     );
 }
